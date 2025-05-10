92d97e657619a6c9542b105b2d395303f4a253b1
M-BintangR <muhbintang650@gmail.com>
2025-05-11 00:40:42 +0800

    tipe data

---
+++ TUGAS-01/tipe-data.php
@@ -0,0 +1,35 @@
+<?php 
+
+// TIPE DATA DESIMAL 
+
+$a = 1234; // desimal
+$b = -1234; // negatif desimal
+
+echo "Nilai a yakini : $a <br/>";
+echo "Nilai b yakini : $b <br/>";
+
+// TIPE DATA DOUBLE
+
+$a = 123.4;
+$b = -1.23;
+
+echo "Nilai a yakini : $a <br/>";
+echo "Nilai b yakini : $b <br/>";
+
+// TIPE DATA STRING
+
+$untai = "Ini lagi belajar php";
+$rantai = "Ini juga belajar database";
+
+echo "Output untai : $untai <br/>";
+echo "Output rantai : $rantai <br/>";
+
+// TIPE DATA BOOLEAN
+
+$ada = true;
+if($ada == true){
+     echo "NIM anda terdaftar dalam database kami";
+}else{
+     echo "Maaf nim anda tidak terdaftar";
+}
+
